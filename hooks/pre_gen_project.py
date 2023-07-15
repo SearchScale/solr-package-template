e5a78166a06bbe1d69f4477acba114a451107fc7
Ishan Chattopadhyaya <ishan@apache.org>
2023-07-16 04:39:56 +0530

    Generate RH, URP and release using a mvn package target

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,19 +3,16 @@ import cookiecutter.prompt
 import os
 import json
 
-'''
-This pre_gen is to avoid to input tests variables if you don't want to create it
-'''
 
 vars = {}
 
 if "{{ cookiecutter.RequestHandler }}" == "True":
     vars["REQUESTHANDLER_CLASS_NAME"] = cookiecutter.prompt.read_user_variable("RequestHandler class name", "MyRequestHandler")
     vars["REQUESTHANDLER_PATH"] = cookiecutter.prompt.read_user_variable("RequestHandler path", "/myrh")
-if "{{ cookiecutter.QueryParser }}" == "True":
-    vars["QUERYPARSER_CLASS_NAME"] = cookiecutter.prompt.read_user_variable("QueryParser class name", "MyQueryParser")
-if "{{ cookiecutter.PostFilter }}" == "True":
-    vars["POSTFILTER_CLASS_NAME"] = cookiecutter.prompt.read_user_variable("PostFilter class name", "MyPostFilter")
+if "{{ cookiecutter.UpdateRequestProcessor }}" == "True":
+    vars["URP_CLASS_NAME"] = cookiecutter.prompt.read_user_variable("UpdateRequestProcessor class name", "MyURP")
+if "{{ cookiecutter.UpdateRequestProcessor }}" == "True":
+    vars["URP_NAME"] = cookiecutter.prompt.read_user_variable("UpdateRequestProcessor simple name:", "my-processor")
 
 with open("vars.json", "w") as outfile:
     json.dump(vars, outfile)
